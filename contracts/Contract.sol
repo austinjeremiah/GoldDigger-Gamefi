16b7d077fe56e0a9e447c57976ab2b1f12824255
austinjeremiah <austinjeremiah04@gmail.com>
2024-11-17 13:51:43 +0530

    Added imports for ERC1155 and ERC20 contracts

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
-}
+import "@thirdweb-dev/contracts/drop/DropERC1155.sol"; // For my collection of Pickaxes
+import "@thirdweb-dev/contracts/token/TokenERC20.sol"; // For my ERC-20 Token contract
+import "@thirdweb-dev/contracts/openzeppelin-presets/utils/ERC1155/ERC1155Holder.sol";
