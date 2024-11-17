e64cd32370a903d0a68707f83f8de82bb4012dab
austinjeremiah <austinjeremiah04@gmail.com>
2024-11-17 14:00:28 +0530

    Created logic for staking

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,5 +2,47 @@
 pragma solidity ^0.8.9;
 
 import "@thirdweb-dev/contracts/drop/DropERC1155.sol"; // For my collection of Pickaxes
-import "@thirdweb-dev/contracts/token/TokenERC20.sol"; // For my ERC-20 Token contract
+import "@thirdweb-dev/contracts/token/TokenERC20.sol"; // For my ERC-20 Token (GOLD)
+
 import "@thirdweb-dev/contracts/openzeppelin-presets/utils/ERC1155/ERC1155Holder.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+
+contract Mining is ReentrancyGuard, ERC1155Holder {
+   
+    DropERC1155 public immutable pickaxeNftCollection;
+    TokenERC20 public immutable rewardsToken;
+
+    constructor(
+        DropERC1155 pickaxeContractAddress,
+        TokenERC20 gemsContractAddress
+    ) {
+        pickaxeNftCollection = pickaxeContractAddress;
+        rewardsToken = gemsContractAddress;
+    }
+
+    struct MapValue {
+        bool isData;
+        uint256 value;
+    }
+     mapping(address => MapValue) public playerPickaxe;
+
+   
+    mapping(address => MapValue) public playerLastUpdate;
+
+    function stake(uint256 _tokenId) external nonReentrant {
+        
+        require(
+            pickaxeNftCollection.balanceOf(msg.sender, _tokenId) >= 1,
+            "You must have at least 1 of the pickaxe you are trying to stake or buy it from the shop"
+        );
+ if (playerPickaxe[msg.sender].isData) {
+            // Transfer using safeTransfer
+            pickaxeNftCollection.safeTransferFrom(
+                address(this),
+                msg.sender,
+                playerPickaxe[msg.sender].value,
+                1,
+                "Returning your old pickaxe"
+            );
+        }
